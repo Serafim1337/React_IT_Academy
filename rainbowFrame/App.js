577ab3a2664b79a5e4d16e547751bb1681a6620e
Serafim Evtihiev <7620226@gmail.com>
2023-07-27 03:17:44 +0300

    feat: implemented rainbow

---
+++ rainbowFrame/App.js
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import './style.css';
+
+import RainbowFrame from "./components/RainbowFrame/RainbowFrame";
+
+let colors = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple'];
+
+ReactDOM.render(
+  <RainbowFrame colorsList={colors}>Hello !</RainbowFrame>,
+  document.querySelector('#container')
+);
