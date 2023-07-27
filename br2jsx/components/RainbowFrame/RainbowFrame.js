55749dd0de298b6dcb890453ee5dc8b683f5d043
Serafim Evtihiev <7620226@gmail.com>
2023-07-27 03:20:09 +0300

    build 1

---
+++ br2jsx/components/RainbowFrame/RainbowFrame.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './RainbowFrame.css';
+
+function RainbowFrame(props) {
+    return createBlocks(props.colorsList, props.children);
+}
+
+RainbowFrame.propTypes = {
+    colorsList: PropTypes.arrayOf(PropTypes.string.isRequired),
+}
+
+function createBlocks(arr, childProp) {
+    let currentBlock = arr.shift();
+    if (currentBlock) {
+        return <div style={{
+            border: '10px ' + currentBlock + ' solid',
+            padding: '10px'
+        }}>
+            {createBlocks(arr, childProp)}
+        </div>
+    } else {
+        return childProp;
+    }
+}
+
+export default RainbowFrame;
+
+
+
